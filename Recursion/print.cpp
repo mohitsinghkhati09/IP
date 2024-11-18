2ed43dfef69b2601bbaa75ea18f58405f12ce1f4
Mohit_Singh_Khati <mohitsinghkhati20@gmail.com>
2024-11-18 06:50:40 +0530

    Update print.cpp

--- Recursion/print.cpp
+++ Recursion/print.cpp
@@ -1,1 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void solve(int n){
+  if(n==1){
+      cout<<1<<" ";
+  }
+  solve(n-1);
+  cout<<n<<" ";
+}
+
+int main(){
+  int n;
+  cin>>n;
+  solve(n);
+  return 0;
+}
