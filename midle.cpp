9ef83dbcc288eab4175ef02b9fee219e1e9fe153
Mohit_Singh_Khati <mohitsinghkhati20@gmail.com>
2025-01-08 10:18:10 +0530

    Create midle.cpp

---
+++ midle.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+//Position this line where user code will be pasted.
+class Solution {
+  public:
+    int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+        // code here
+        vector<int>ans(arr1.begin(),arr1.end());
+        ans.insert(ans.end(),arr2.begin(),arr2.end());
+        
+        
+        sort(ans.begin(),ans.end());
+        
+        int n = ans.size();
+        int mid = n/2;
+        int a = (ans[mid-1] + ans[mid]);
+        return a;
+        
+    }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function template in C++
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, brr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            brr.push_back(number2);
+        }
+        Solution ob;
+        int res = ob.SumofMiddleElements(arr, brr);
+        cout << res << endl << "~\n";
+    }
+}
+// } Driver Code Ends
