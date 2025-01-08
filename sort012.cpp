44f508d19ad445fa50f2096c43efa0fc47d72053
Mohit_Singh_Khati <mohitsinghkhati20@gmail.com>
2025-01-08 09:53:25 +0530

    Create sort012.cpp

---
+++ sort012.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        
+        
+        int n = arr.size();
+        int l = 0;
+        int mid =0;
+        int h = n-1;
+        
+        
+        while(mid<=h){
+            if(arr[mid]==0){
+                swap(arr[l++],arr[mid++]);
+            }
+            else if(arr[mid]==1){
+                mid++;
+                
+            }
+            else{
+                swap(arr[mid],arr[h--]);
+            }
+        }
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
